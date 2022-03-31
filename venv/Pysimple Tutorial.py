b3b824bafe5da1b09f685d3ccf26ad1f997552e4
Michael Swint <ms2429@mcla.edu>
2022-03-31 04:04:57 -0400

    1.0.2

--- venv/Pysimple Tutorial.py
+++ venv/Pysimple Tutorial.py
@@ -1,18 +1,39 @@
 import PySimpleGUI as sg
 
+
+#Set the theme for the gui (text, colors background, colors etc.)
 sg.theme("DarkAmber")
 
-layout=[[sg.Text("Enter Name")],
-        [sg.InputText(key='-Name-')],
-        [sg.Text("Enter Radius")],
-        [sg.InputText(key='-Radius-')],
+#This is the contents of the window!
+layout=[[sg.Text("Enter the chance in decimal format (%1 = .01)")],
+        [sg.InputText(key='-Chance-')],
+        [sg.Text("Enter the series(number of runs)")],
+        [sg.InputText(key='-Runs-')],
         [sg.Submit(), sg.Cancel()],
 ]
+#The contents are wraped within the window.
+window = sg.Window('Chance Calculator', layout)
 
-window = sg.Window('First Window', layout)
-
+#Grab the user input.
 event, values = window.read()
 
+#Close the main menu to free up thread.
 window.close()
 
-sg.popup('You entered', values['-Name-'])
+#Function to calculate the chance.
+def calc_chance(series, per):
+       chance = 1-((1-per)**series)
+       return chance
+
+#User input is currently type string, this converts to float.
+float_Chance = float(values['-Chance-'])
+float_Runs = float(values['-Runs-'])
+
+#Pass the values to calc_chance function
+chance = calc_chance(float_Runs, float_Chance)
+
+#Create a 'popup' window with the completed calculation!
+sg.popup(F"The chance of an event occuring at least once over {values['-Runs-']} runs is {chance:.10%}")
+
+
+
